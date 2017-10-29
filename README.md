365704775f7b42060c79114ca6e43bb3b1c6f51c
Patrick Goldinger <patrick.goldinger.02@gmail.com>
2017-10-29 23:29:32 +0100

    1st version of countdown
    1st version of the newyear countdown.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # newyear-countdown
+
+Web app that counts down to next January 1st. It selects automatically the next year (based on local system time). Background images are made with svg inline tags.
---
+++ countdown.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+    // define references to elements
+    var elements = {
+        cd: document.getElementById("cd"),
+        cd_title: document.getElementById("cd-title"),
+        cd_days: document.getElementById("cd-days"),
+        cd_hours: document.getElementById("cd-hours"),
+        cd_mins: document.getElementById("cd-mins"),
+        cd_secs: document.getElementById("cd-secs"),
+        cd_timetil: document.getElementById("cd-timetil")
+    };
+
+    elements.cd_title.innerHTML += " " + ((new Date()).getFullYear()+1);
+
+    // Current year + 1, Jan, 1st
+    var endDate = new Date((new Date()).getFullYear() + 1 + "-1-1"),
+        _sec = 1000,
+        _min = _sec*60,
+        _hour = _min*60,
+        _day = _hour*24;
+
+    elements.cd_timetil.innerHTML = "Time until " + endDate.toDateString();
+
+    setInterval(function(){
+        var nowDate = new Date(),
+            dif = endDate.getTime() - nowDate.getTime();
+        if(dif < 0){
+            clearInterval();
+            elements.cd_title.innerHTML = "Happy New Year!";
+        }
+        var days = Math.floor(dif/_day),
+            hours = Math.floor(dif%_day/_hour),
+            mins = Math.floor(dif%_hour/_min);
+            secs = Math.floor(dif%_min/_sec);
+        elements.cd_days.innerHTML = ("00" + days).slice(-2);
+        elements.cd_days.nextElementSibling.innerHTML = "Day" + (days == 1 ? "" : "s");
+        elements.cd_hours.innerHTML = ("00" + hours).slice(-2);
+        elements.cd_hours.nextElementSibling.innerHTML = "Hour" + (hours == 1 ? "" : "s");
+        elements.cd_mins.innerHTML = ("00" + mins).slice(-2);
+        elements.cd_mins.nextElementSibling.innerHTML = "Minute" + (mins == 1 ? "" : "s");
+        elements.cd_secs.innerHTML = ("00" + secs).slice(-2);
+        elements.cd_secs.nextElementSibling.innerHTML = "Second" + (secs == 1 ? "" : "s");
+    }, 1000);
+}
